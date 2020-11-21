1e9399f9185386aa1f6d605f12b32949b9dd787e
intansemc2 <intansemc2@gmail.com>
2020-11-21 12:13:10 +0700

    Cap nhat kieu du lieu luu ma buu chinh va ma tinh thanh  pho

--- Tạo dữ liệu đối tác.js
+++ Tạo dữ liệu đối tác.js
@@ -1,2 +1,36 @@
+const fs = require('fs');
+
+const CSV = require('./csv');
+
 const maBuuChinh = require('./Dữ liệu về mã bưu chính ở Việt Nam');
 const maTinhTP = require('./Dữ liệu về mã tỉnh thành phố của Việt Nam trên Odoo');
+
+const taoDiaChi = require('./Tạo một địa chỉ ngẫu nhiên');
+const taoTenMotNguoi = require('./Tạo tên của một người ngẫu nhiên');
+const taoSDT = require('./Tạo một số điện thoại ngẫu nhiên');
+
+const CSV_COLUMNS = ['name', 'street', 'street2', 'city', 'state_id', 'zip', 'country_id', 'phone', 'mobile', 'email', 'website', 'lang'];
+
+const SO_LUONG = 10000;
+
+let duLieuBang = [];
+for (let i = 0; i <= SO_LUONG; i += 1) {
+    let name = taoTenMotNguoi.taoTenNguoiTheoSoLuongTuDong((tenNguoiMoi) => duLieuBang.find((d) => d[0] == tenNguoiMoi));
+    let street = '';
+    let street2 = '';
+    let city = '';
+    let state_id = '';
+    let zip = '';
+    let country_id = '';
+    let phone = '';
+    let mobile = '';
+    let email = '';
+    let website = '';
+    let lang = '';
+
+    let duLieuMoi = [name, street, street2, city, state_id, zip, country_id, phone, mobile, email, website, lang];
+    duLieuBang.push(duLieuMoi);
+}
+duLieuBang.unshift(CSV_COLUMNS);
+
+fs.writeFileSync('Kết qủa danh sách thông tin đối tác.csv', CSV.stringify(duLieuBang), { encoding: 'utf-8' });
